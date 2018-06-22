2e0918d9ae0832460d511b5bbb6a79b7df9c1829
Jaewe Heo <jaeweheo@gmail.com>
2018-06-22 18:43:11 +0900

    Create GreetingExt

---
+++ buildSrc/src/main/kotlin/importre/GreetingExt.kt
@@ -0,0 +1,10 @@
+package importre
+
+/**
+ * [GreetingPlugin] Extension
+ *
+ * @property userName for **greeting**
+ */
+open class GreetingExt(
+    var userName: String? = null
+)
--- buildSrc/src/main/kotlin/importre/GreetingPlugin.kt
+++ buildSrc/src/main/kotlin/importre/GreetingPlugin.kt
@@ -6,18 +6,19 @@ import org.gradle.kotlin.dsl.invoke
 
 class GreetingPlugin : Plugin<Project> {
 
-    private fun greeting(): String {
-        return "Hello, World!"
+    private fun greeting(userName: String?): String {
+        return "Hello, ${userName ?: "World"}!"
     }
 
     override
     fun apply(project: Project): Unit = project.run {
+        val ext = extensions.create("greeting", GreetingExt::class.java)
         tasks {
             "greeting" {
                 group = "kotlin-flavored"
                 description = "코틀린 맛 그래들 플러그인"
                 doLast {
-                    greeting().run(::println)
+                    greeting(ext.userName).run(::println)
                 }
             }
         }
